2c22e8a61f9c15f531e5712d1be8afcba911ee69
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-11-19 15:19:58 +0200

    Run a 5 round game and ignore draws

--- app.js
+++ app.js
@@ -25,33 +25,83 @@ function getPlayerChoice () {
     } 
 }       
 // When GUI will be created make 'const playerSelection = prompt("Make your move").toLowerCase();' instead of getPlayerChoice function.
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+
 
 
 function playRound(playerSelection, computerSelection) {
+    
     if (playerSelection === computerSelection) {
-        return `It's a tie! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with a ${computerSelection}`
+        console.log(`It's a tie! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with a ${computerSelection}`);
+        return "Tie"
+
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "You Win! Rock beats Scissors"
+        console.log("You Win! Rock beats Scissors");
+        return "Win";
+
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "You Lose! Rock beats Scissors"
+        console.log("You Lose! Rock beats Scissors")
+        return "Lose";
+
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "You Win! Paper beats Rock"
+        console.log("You Win! Paper beats Rock");
+        return "Win";
+
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        return "You Lose! Paper beats Rock"
+        console.log("You Lose! Paper beats Rock");
+        return "Lose";
+
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "You Win! Scissors beats Paper"
+        console.log("You Win! Scissors beats Paper");
+        return "Win";
+
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "You Lose! Scissors beats Paper"
+        console.log("You Lose! Scissors beats Paper");
+        return "Lose";
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
 
 //Compares the paramaeters and return the result - for example, "You Lose! Paper beats Rock". 
+// Done
 
+//Write a function called game()
+//
 
+function game() {
+    
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (true) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+
+        let result = playRound(playerSelection, computerSelection);
+
+        if (result === "Win") {
+            playerScore += 1;
+            
+            if (playerScore == 5) {
+                console.log("You win!");
+                console.log(`Player: ${playerScore} Computer: ${computerScore}`)
+                return false;
+            }
+        } else if (result === "Lose") {
+            computerScore += 1; 
+            
+            if (computerScore == 5) {
+                console.log("You lose!");
+                console.log(`Player: ${playerScore} Computer: ${computerScore}`)
+                return false;
+            }
+        } else {
+            continue;
+        }
+    }    
+    
+}
+
+game()
 
 
 
