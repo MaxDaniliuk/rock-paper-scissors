a1ffa7b1d879e4531cc64c4cb11c8f9137545057
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-12-03 12:34:07 +0200

    Create UI, listen to button and display the result

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-console.log("Rock-Paper-Scissors Game");
-
-
-// Get the computer to make a choice
-// Computer should randomly return ‘Rock’, ‘Paper’ or ‘Scissors’
-// Done ! 
 
 function getComputerChoice() {
     const computerChoice = ["rock", "paper", "scissors"];
     return computerChoice[Math.floor(Math.random() * computerChoice.length)]
 }
 
-// Play a single round
-// Here, you have two parameters, one is yours and the second is computer's
-// Yours choice must be case insensitive
-
 function getPlayerChoice () {
+    let btns = document.querySelectorAll('.btn');
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', function() {    
+        playRound(this.innerText.toLowerCase(), getComputerChoice())
+            
+            
+        })
+    });
+    /*
     let playerChoice = prompt("Make your move").toLowerCase();
     while (true) {
         if (playerChoice === "rock" || playerChoice === "scissors" || playerChoice === "paper") {
@@ -23,59 +23,60 @@ function getPlayerChoice () {
             playerChoice = prompt("Enter Rock, Paper or Scissors").toLowerCase();
         }  
     } 
-}       
+    */
+} 
+
+function getRoundResult (str) {
+    let roundOutput = document.querySelector('.round-output');
+    roundOutput.innerHTML = "";
+    roundOutput.appendChild(document.createTextNode(str));
+}
+
 // When GUI will be created make 'const playerSelection = prompt("Make your move").toLowerCase();' instead of getPlayerChoice function.
-
-
 
 function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === computerSelection) {
-        console.log(`It's a tie! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} ties with a ${computerSelection}`);
-        return "Tie"
+        getRoundResult("It's a tie!");
+        //return "Tie"
+        
 
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("You Win! Rock beats Scissors");
-        return "Win";
+        getRoundResult("You Win! Rock beats Scissors");
+        //return "Win";
 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("You Lose! Rock beats Scissors")
-        return "Lose";
+        getRoundResult("You Lose! Scissors are defeated by Rock")
+        //return "Lose";
 
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("You Win! Paper beats Rock");
-        return "Win";
+        getRoundResult("You Win! Paper beats Rock");
+        //return "Win";
 
     } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("You Lose! Paper beats Rock");
-        return "Lose";
+        getRoundResult("You Lose! Rock is defeated by Paper");
+        //return "Lose";
 
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("You Win! Scissors beats Paper");
-        return "Win";
+        getRoundResult("You Win! Scissors beats Paper");
+        //return "Win";
 
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("You Lose! Scissors beats Paper");
-        return "Lose";
+        getRoundResult("You Lose! Paper is defeated by Scissors");
+        //return "Lose";
     }
 }
 
+getPlayerChoice()
 
-//Compares the paramaeters and return the result - for example, "You Lose! Paper beats Rock". 
-// Done
-
-//Write a function called game()
-//
-
-function game() {
-    
+/*
+function playGame() {
     let playerScore = 0;
     let computerScore = 0;
 
     while (true) {
         const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
-
         let result = playRound(playerSelection, computerSelection);
 
         if (result === "Win") {
@@ -98,11 +99,10 @@ function game() {
             continue;
         }
     }    
-    
 }
 
-game()
-
+playGame()
+*/
 
 
 
